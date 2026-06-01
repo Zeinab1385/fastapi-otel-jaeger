c9695f65b370d61416555c8028c26c9180b1ccf8
Zeinab1385 <zeinab.hoseiny1385@gmail.com>
2026-06-01 13:44:06 +0330

    Fix cpu metric callback
    Added Prometheus metrics for user registration and CPU usage.

--- api.py
+++ api.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from service import UserServices
-
+from opentelemetry import metrics
+from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.exporter.prometheus import PrometheusMetricReader
+from prometheus_client import start_http_server
 from opentelemetry import trace
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter
@@ -23,19 +26,58 @@ processor = BatchSpanProcessor(local_jaeger_exporter)
 provider.add_span_processor(processor)
 trace.set_tracer_provider(provider)
 
-FastAPIInstrumentor.instrument_app(app)
+
+reader = PrometheusMetricReader()
+meter_provider = MeterProvider(resource=resource, metric_readers=[reader])
+metrics.set_meter_provider(meter_provider)
+
+meter = metrics.get_meter("api.metrics")
+
+register_counter = meter.create_counter(
+    name="user_registration_total",
+    description="Total number of users registered",
+    unit="1"
+)
+
+def cpu_check_callback(options):
+    import random
+    yield metrics.Observation(random.randint(15, 40))
+
+meter.create_observable_gauge(
+    name="system_cpu_usage",
+    description="Asynchronous observation of CPU usage",
+    callbacks=[cpu_check_callback],
+    unit="%",
+)
+
+FastAPIInstrumentor.instrument_app(app, meter_provider=meter_provider)
+
+start_http_server(8042)
+
+
+@app.on_event("startup")
+async def initialize_metrics():
+    register_counter.add(0, {"status": "success"})
+    register_counter.add(0, {"status": "failed", "reason": "validation"})
+    register_counter.add(0, {"status": "failed", "reason": "server_error"})
+    print("🚀 CANTER INITIALIZED SUCCESSFULLY!")
+
 
 class UserRegisterSchema(BaseModel):
     name: str
     email: str
 
+
 @app.post("/register")
 def register(user: UserRegisterSchema):
     try:
         result = user_services.register_new_user(user.model_dump())
+        register_counter.add(1, {"status": "success"})
         return {"status": "success", "data": result}
     except ValueError as e:
+        register_counter.add(1, {"status": "failed", "reason": "validation"})
         raise HTTPException(status_code=400, detail=str(e))
     except Exception as e:
+        register_counter.add(1, {"status": "failed", "reason": "server_error"})
         print("ERROR LOG:", e)
         raise HTTPException(status_code=500, detail="Internal server error")
